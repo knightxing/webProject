ad81d60957025a439081ddc9d6f0ae09e07c0ec1
chx <928946042@qq.com>
2016-11-01 02:24:45 +0800

    111

--- javatest/src/com/xing/helloWorld.java
+++ javatest/src/com/xing/helloWorld.java
@@ -7,6 +7,8 @@ public class helloWorld {
 		System.out.println("i am javatest1");
 		
 		System.out.println("i am a master ...");
+		
+		System.out.println("不进行本地提交。。。");
 
 		System.out.println("不进行本地提交。。。");
 	}
