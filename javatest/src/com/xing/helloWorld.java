13d03b840dafd40c46cfb7d7e470a5ac069df88f
chx <928946042@qq.com>
2016-11-01 02:39:55 +0800

    222

--- javatest/src/com/xing/helloWorld.java
+++ javatest/src/com/xing/helloWorld.java
@@ -3,7 +3,7 @@ package com.xing;
 public class helloWorld {
 
 	public static void main(String[] args) {
-
+qqq
 		System.out.println("i am javatest1");
 		
 		System.out.println("i am a master ...");
