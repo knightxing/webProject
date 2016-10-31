f0f177ce6734a8417fc7f03476a94474f58eb194
chx <928946042@qq.com>
2016-11-01 01:17:55 +0800

    合并114分支

--- javatest/src/com/xing/helloWorld.java
+++ javatest/src/com/xing/helloWorld.java
@@ -4,7 +4,7 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("i am javatest");
+		System.out.println("i am javatest1");
 	}
 
 }
